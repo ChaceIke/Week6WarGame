0c588902b3f2b898134b3d255b6892f2093903b5
unknown <IkeChace@gmail.com>
2023-04-17 05:27:15 -0700

    hee hee 2

---
+++ indextest.js
@@ -0,0 +1,23 @@
+const expect = chai.expect;
+
+describe("Deck", function() {
+    describe("constructor", function() {
+        it("should create a full deck of 52 cards", function() {
+            const deck = new Deck();
+            expect(deck.cards).to.have.lengthOf(52);
+        });
+    });
+
+    describe("shuffleCards", function() {
+        it("should shuffle the cards array", function() {
+            const deck = new Deck();
+            const war = new War();
+            const originalCards = [...deck.cards];
+            war.shuffleCards();
+            expect(deck.cards).to.not.equal(originalCards);
+        });
+    });
+});
+
+console.log(deck.cards);
+console.log(originalCards);
